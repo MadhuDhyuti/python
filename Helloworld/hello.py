43996c3dba49d1171e692257423d7fe3c2cf8d86
oracl <oracl@DESKTOP-S7K52RL.attlocal.net>
2018-04-25 21:27:03 -0500

    Initial commit

---
+++ Helloworld/hello.py
@@ -0,0 +1,21 @@
+'''
+Created on Apr 17, 2018
+
+@author: Oracle
+'''
+print("Hello World");
+
+1+1
+2+2
+3+4
+
+#My first if loop
+
+x=False
+y=100
+
+if x:
+    print("Welcome")
+elif (y<101):
+    print("Hundred")
+    
